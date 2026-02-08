17945f27250c13289c9cd211a96c18e810e799e2
ycking0823 <114500092+ycking0823@users.noreply.github.com>
2026-02-07 19:05:14 -0800

    Add files via upload

---
+++ report_generator.py
@@ -0,0 +1,17 @@
+import sys
+from typing import List, Dict, Tuple
+
+def tokenize(text_file_path: str) -> List[str]: # List<Token>
+    pass
+
+def compute_word_frequencies(tokens: List[str]) -> Dict[str, int]: # Map<Token,Count>
+    pass
+
+def print_frequencies(frequencies: Dict[str, int]) -> None:
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
